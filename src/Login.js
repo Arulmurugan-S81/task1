71eb5626d57e7610adde03a8a7af7492d73a0142
Arulmurugan-S81 <aruls8031@gmail.com>
2025-03-20 16:00:31 +0530

    Added contact form with validation

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,29 @@
-import { Link } from "react-router-dom";
-const Login=()=>{
-    return(
-<div className="login-container">
-    <div>
-    <h2>Login</h2>
-    <input>username</input>
-    <input>password</input>
-    <button>Login</button>
+import React from "react";
+import './Login.css';
+
+const LoginPage = () => {
+  return (
+    <div className="container1">
+    <div className="container">
+      <div className="card">
+        <h2 className="title">Login</h2>
+        <div className="input-container">
+          <label>Username</label>
+          <input type="text" className="input" />
+        </div>
+        <div className="input-container">
+          <label>Password</label>
+          <input type="password" className="input" />
+        </div>
+        <p className="forgot-password">Forget Password?</p>
+        <button className="button">Login</button>
+        <p className="signup">
+          Not a Member? <span className="signup-link">Signup</span>
+        </p>
+      </div>
     </div>
-</div>
-    );
-}
-export  default Login;
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Home";
-import Blogs from "./Blog";
+import Blog from "./Blog";
 import About from "./About";
 import Contact from "./Contact";
 import Fruniture from "./Fruniture";
@@ -12,17 +12,16 @@ import Login from "./Login";
 import "./index.css";
 
 export default function App() {
-  const [cart, setCart] = useState([]);
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs/>} />  {/* Pass cart & setCart */}
+          <Route path="blog" element={<Blog />} />
           <Route path="contact" element={<Contact />} />
           <Route path="about" element={<About />} />
-          <Route path="fruniture" element={<Fruniture />} />  {/* Pass cart & setCart */}
+          <Route path="furniture" element={<Fruniture />} />
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
